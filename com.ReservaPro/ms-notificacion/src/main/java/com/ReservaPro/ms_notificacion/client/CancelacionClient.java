126afdeac9af50e48ca8bf26431f75da8de3b556
cindia17 <216095690+cindia17@users.noreply.github.com>
2026-06-21 17:20:08 -0400

    log en historial resercva

--- com.ReservaPro/ms-notificacion/src/main/java/com/ReservaPro/ms_notificacion/client/CancelacionClient.java
+++ com.ReservaPro/ms-notificacion/src/main/java/com/ReservaPro/ms_notificacion/client/CancelacionClient.java
@@ -3,10 +3,7 @@ package com.ReservaPro.ms_notificacion.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-@FeignClient(
-        name = "ms-cancelacion",
-        url = "http://localhost:8082"
-)
+@FeignClient(name = "ms-cancelacion")
 public interface CancelacionClient {
 
     @GetMapping("/api/v1/cancelaciones/{id}")
